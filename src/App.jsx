9c4193c02408b09517ad2a1529017df57eebf538
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2023-06-25 18:42:28 +0300

    До инпута

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import styles from './App.module.css';
 export const App = () => {
 	const [toDos, setToDos] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [isCreating, setIsCreating] = useState(false);
+	const [isUpdating, setIsUpdating] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
 	const [refreshToDosFlag, setRefreshToDosFlag] = useState();
+
+	const refreshToDos = () => setRefreshToDosFlag(!refreshToDosFlag);
 
 	useEffect(() => {
 		setIsLoading(true);
@@ -15,8 +20,9 @@ export const App = () => {
 				setToDos(loadedToDos);
 			})
 			.finally(() => setIsLoading(false));
-	}, []);
+	}, [refreshToDosFlag]);
 	const requestAddDeal = () => {
+		setIsCreating(true);
 		fetch('http://localhost:3004/todos', {
 			method: 'Post',
 			headers: { 'Content-Type': 'application/json;charset=utf-8' },
@@ -27,7 +33,38 @@ export const App = () => {
 			.then((rawResponse) => rawResponse.json())
 			.then((response) => {
 				console.log('Добавлено новое дело:', response);
-			});
+				refreshToDos();
+			})
+			.finally(() => setIsCreating(false));
+	};
+	const requestUpdateDeal = () => {
+		setIsUpdating(true);
+		fetch('http://localhost:3004/todos/4', {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json;charset=utf-8' },
+			body: JSON.stringify({
+				name: `Купить яблок ${Math.random()} кг`,
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then((response) => {
+				console.log('Дело обновлено:', response);
+				refreshToDos();
+			})
+			.finally(() => setIsUpdating(false));
+	};
+	const requestDeleteDeal = () => {
+		setIsDeleting(true);
+		fetch('http://localhost:3004/todos/4', {
+			method: 'DELETE',
+			headers: { 'Content-Type': 'application/json;charset=utf-8' },
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then((response) => {
+				console.log('Дело обновлено:', response);
+				refreshToDos();
+			})
+			.finally(() => setIsDeleting(false));
 	};
 	return (
 		<div className={styles.app}>
@@ -40,7 +77,26 @@ export const App = () => {
 					</div>
 				))
 			)}
-			<button onClick={requestAddDeal}>Добавить дело</button>
+			<form className={styles.forms}>
+				<label>
+					Введите новую задачу
+					<input type="text" name="name" />
+				</label>
+				<input
+					type="submit"
+					disabled={isCreating}
+					onClick={requestAddDeal}
+					value="Добавить задачу"
+				/>
+			</form>
+
+			<button disabled={isUpdating} onClick={requestUpdateDeal}>
+				Изменить дело
+			</button>
+			<button disabled={isDeleting} onClick={requestDeleteDeal}>
+				Удалить дело
+			</button>
+			{/* <input>Введите задачу</input> */}
 		</div>
 	);
 };
